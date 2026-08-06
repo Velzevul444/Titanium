abc73b5a7236b54b441ad81ae1e50b27d985ff90
Velzevul <maximilianus06@gmail.com>
2026-08-06 18:58:03 +0300

    qwe

--- react/RUser.jsx
+++ react/RUser.jsx
@@ -1,10 +1,13 @@
 import React, { useState } from "react";
 
 const RUser = () => {
+
+
 	const [showAuth, setShowAuth] = useState(false);
 	const autOff = () => {
 		setShowAuth(!showAuth);
 	};
+
 	const [showLog, setShowLog] = useState(false);
 	const logOff = () => {
 		setShowLog(!showLog);
@@ -32,19 +35,28 @@ const RUser = () => {
 
 			{showAuth && (
 				<div className="Authorization">
-					<div className="Login" onClick={() => logOff()}>Login or</div>
-					<div className="Registration" onClick={() => logOff()}>Registration</div>
+					<div style={{top: showLog ? "7% ": "12%",}} className="Login" onClick={() => logOff()}>Login or</div>
+					<div style={{top: showLog ? "7% ": "12%",}} className="Registration" onClick={() => logOff()}>Registration</div>
 					<input
+						style={{top: showLog ? "30% ": "35%",}}
 						className="Email"
 						type="Email"
 						placeholder="&nbsp;Email"
 					/>
 					<input
+						style={{top: showLog ? "50% ": "55%",}}
 						className="Password"
 						type="Password"
 						placeholder="&nbsp;Password"
 					/>
-					<button className={"ButtonL"}>Login</button>
+					{showLog && (
+						<input
+							className="REpassword"
+							type="password"
+							placeholder="Repeat password"
+						/>
+					)}
+					<div style={{top: showLog ? "90% ": "75%",}} className={"ButtonL"}>{showLog ? "Registration" : "Login"}</div>
 
 				</div>
 			)}
