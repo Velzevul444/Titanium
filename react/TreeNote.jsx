186b41287ffbbce5a2303ea12585fde5e5f25f60
Velzevul <maximilianus06@gmail.com>
2026-08-07 17:17:41 +0300

    qwe

--- react/TreeNote.jsx
+++ react/TreeNote.jsx
@@ -1,11 +1,27 @@
+import React, {useState} from "react";
 const TreeNote = () => {
+
+
+	const [NewCore, setNewElectron] = useState(true);
+	const choice = () => {
+			setNewElectron(!NewCore);
+	}
+
+	const [PutColor, setPutColor] = useState("true");
+	const SColor = () => {
+		setPutColor(!PutColor);
+	}
+
+
 	return (
 		<div className="treeNote">
-			<div className={"NewCore"}>New core</div>
-			<div className={"NewElectron"}>New electron</div>
+			<div style={{ border: NewCore ? "2px solid white" : "none" }} className={"NewCore"} onClick={() => choice()}>New core</div>
+			<div style={{ border: NewCore ? "none" : "2px solid white" }} className={"NewElectron"} onClick={() => choice()}>New electron</div>
 			<div className={"NewBond"}>Bond notes</div>
-			<div className={"SetColor"}>Set color to note </div>
-
+			<div className={"SetColor"} onClick={() => SColor()}>Set color to note </div>
+			{PutColor && (
+				<input type={"color"} className={"PutColor"} />
+			)}
 		</div>
 	)
 }
