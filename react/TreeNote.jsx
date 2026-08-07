d9ad7bb931bc3517df472fdacd7cc8601a5bdc35
Velzevul <maximilianus06@gmail.com>
2026-08-07 15:34:26 +0300

    qwe

--- react/TreeNote.jsx
+++ react/TreeNote.jsx
@@ -1,6 +1,12 @@
 const TreeNote = () => {
 	return (
-		<div className="treeNote"></div>
+		<div className="treeNote">
+			<div className={"NewCore"}>New core</div>
+			<div className={"NewElectron"}>New electron</div>
+			<div className={"NewBond"}>Bond notes</div>
+			<div className={"SetColor"}>Set color to note </div>
+
+		</div>
 	)
 }
 export default TreeNote;
